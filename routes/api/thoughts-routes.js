b2da96f43f04188a79400bede178aa8cc95d4436
Francella Aguilar <francellaaguilar@Francellas-MBP.attlocal.net>
2022-05-02 20:44:07 -0400

    adding thoughts-route.js

---
+++ routes/api/thoughts-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    getThoughtsById,
+    createThoughts,
+    updateThoughts,
+    deleteThoughts,
+    addReaction,
+    deleteReaction
+} = require('../../controllers/thoughts-controller');
+
+// /api/thoughts
+router
+.route('/')
+.get(getAllThoughts)
+.post(createThoughts);
+
+// /api/thoughts/:id
+router
+.route('/:id')
+.get(getThoughtsById)
+.post(updateThoughts)
+.delete(deleteThoughts);
+
+// /api/thoughts/:thoughtId/reactions
+router
+.route('/:thoughtId/reactions')
+.post(addReaction);
+
+router
+.route('/:thoughtId/reactions/:reactionId')
+.delete(deleteReaction);
+
+module.exports = router;
